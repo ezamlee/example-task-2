a4b78aec77ed8c78d4d844e171c3ce9e88657ee1
Ezamlee <sam@localhost.localdomain>
2019-09-05 01:37:58 +0200

    task 2 completed

--- client/src/map_component.js
+++ client/src/map_component.js
@@ -1,7 +1,7 @@
 /* global fetch, L */
 import React, { useEffect, useRef, useState } from 'react'
 import Moment from 'moment'
-
+let marker = undefined
 function getRandomColor() {
   var letters = '0123456789ABCDEF';
   var color = '#';
@@ -18,15 +18,23 @@ const getRouteSummary = (locations) => {
 const MapComponent = () => {
   const map = useRef()
   const [locations, setLocations] = useState()
+  const [timeValue, setTimeValue] = useState("")
+  const [min, setMin] = useState()
+  const [max, setMax] = useState()
+  const [latLong, setLatLong] = useState()
   // Request location data.
+
   useEffect(() => {
     fetch('http://localhost:3000')
       .then(response => response.json())
       .then((json) => {
         setLocations(json)
+        setMin(Moment(json[json.length - 1][json[json.length - 1].length - 1].time).unix())
+        setMax(Moment(json[0][0].time).unix())
+        setTimeValue(Moment(json[0][0].time).unix()*1000)
       })
+
   }, [])
-  // TODO(Task 2): Request location closest to specified datetime from the back-end.
 
   // Initialize map.
   useEffect(() => {
@@ -38,6 +46,7 @@ const MapComponent = () => {
       maxZoom: 18,
       attribution
     })
+    
     map.current.setView(new L.LatLng(52.51, 13.40), 9)
     map.current.addLayer(osm)
   }, [])
@@ -47,21 +56,56 @@ const MapComponent = () => {
       return // If map or locations not loaded yet.
     }
     // TODO(Task 1): Replace the single red polyline by the different segments on the map.
-     locations.map((loc) => {
+    locations.map((loc) => {
       const latlons = loc.map(({ lat, lon }) => [lat, lon])
       const polyline = L.polyline(latlons, { color: getRandomColor() }).bindPopup(getRouteSummary(loc)).addTo(map.current)
       map.current.fitBounds(polyline.getBounds())
       return () => map.current.remove(polyline)
     })
-  }, [locations, map.current] )
 
-  // TODO(Task 2): Display location that the back-end returned on the map as a marker.
+    
+  }, [locations, map.current])
+
+
+  useEffect(() => {
+    if (timeValue) {
+      if (marker) {
+        marker.setLatLng(latLong);
+      }
+      else {
+        marker = L.marker(latLong);
+        map.current.addLayer(marker);
+        
+      }
+      map.current.setView(new L.LatLng(latLong[0], latLong[1]))
+    }
+  },[latLong])
 
   return (
     <div>
       {locations && `${locations.length} locations loaded`}
       {!locations && 'Loading...'}
       <div id='mapid' />
+      <span >{Moment(min * 1000 || 0).format("dddd, MMMM YYYY Do, h:mm a")}</span>
+      <input
+        style={{ width: '70%' }}
+        type="range"
+        min={min * 1000 || 0}
+        max={max * 1000 || 0}
+        step={60 * 5 *1000}
+        value={timeValue}
+        onChange={(e) => {
+          setTimeValue(parseInt(e.target.value));
+          if(timeValue)
+          fetch(`http://localhost:3000/location/${timeValue}`)
+            .then(response => response.json())
+            .then((json) => {
+              setLatLong(json)
+            })
+        }}
+      />
+      <span >{Moment(max * 1000 || 0).format("dddd, MMMM YYYY Do, h:mm a")}</span>
+      <div style={{ margin: "0 40%" }}><p>{Moment(timeValue).format("dddd, MMMM YYYY Do, h:mm a")}</p></div>
     </div>)
 }
 
--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 const splitIndex = 13;
-app.use((req, res, next) => {
+
+
+app.use((_req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
   next()
@@ -9,8 +11,7 @@ app.use((req, res, next) => {
 
 const exampleData = require('../data/tracking.json')
 
-app.get('/', (req, res) => {
-  // TODO(Task 1): Split tracking data into trip segments for example by using the time property.
+function chunckData() {
   let data = {}
   exampleData.map((item) => {
     if (!data[item.time.substr(0, splitIndex)])
@@ -18,12 +19,20 @@ app.get('/', (req, res) => {
     else
       data[item.time.substr(0, splitIndex)].push(item)
   })  
-  res.send((Object.values(data)))
+  return Object.values(data);
+}
+
+app.get('/', (_req, res) => {
+  // TODO(Task 1): Split tracking data into trip segments for example by using the time property.
+  res.send(chunckData());
 })
 
 app.get('/location/:when', (req, res) => {
   // TODO(Task 2): Return the tracking data closest to `req.params.when` from `exampleData`.
-  res.send({})
+  
+  const {lon,lat} =  exampleData.filter( (item) => ! ( new Date(item.time) > new Date(parseInt(req.params.when)) ))[0]
+  
+  res.send([lat,lon])
 })
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
